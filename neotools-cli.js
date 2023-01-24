7bb4f46aa896670b4cccdde1e705c9148b255f2f
naguerovalencia <naguerovalencia@gmail.com>
2023-01-24 16:13:46 -0300

    new features: add-logs add-helpers

--- neotools-cli.js
+++ neotools-cli.js
@@ -1,4 +1,7 @@
+
 const args = process.argv.slice(2);
 args.forEach(arg => {
-    if (arg == 'start') require('./lib/start.js')()
+    if (arg == 'full') require('./lib/start.js')()
+    if (arg == 'add-logs') require('./lib/functions.js').logger()
+    if (arg == 'add-helpers') require('./lib/functions.js').addCommonFunctions()
 })
