e82aecd2c32add6853c27c3a5fde713619134cc9
naguerovalencia <naguerovalencia@gmail.com>
2023-01-25 01:35:42 -0300

    features: generate routes, models and controllers

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const args = process.argv.slice(2);
+const readline = require('readline');
 
 if (args[0] == 'start') require('../lib/start.js')()
 else if (args[0] == '--add') {
@@ -10,16 +11,76 @@ else if (args[0] == '--add') {
         if (args[x] == 'dependences') require('../lib/functions.js').addDependences()
     }
 }
-else if (args[0] == '--help') {
+else if (args[0] == '--version' || args[0] == '-v') {
+    let package = require('../package.json');
+    console.log(`NeøTools v${package.version}`)
+}
+else if (args[0] == '--generate' || args[0] == '-g') {
+    if (args[1] == 'model' || args[1] == 'm') {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question('Ingrese el nombre del modelo: ', (modelName) => {
+            require('../lib/functions.js').model(modelName)
+            rl.close();
+        });
+    }
+    else if (args[1] == 'controller' || args[1] == 'c') {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question('Ingrese el nombre del controlador: ', (controllerName) => {
+            require('../lib/functions.js').controller(controllerName)
+            rl.close();
+        });
+    }
+    else if (args[1] == 'route' || args[1] == 'r') {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question('Ingrese el nombre de la ruta: ', (routeName) => {
+            require('../lib/functions.js').route(routeName)
+            rl.close();
+        });
+    }
+    else if (args[1] == 'all' || args[1] == 'a') {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question('Ingrese el nombre de los elementos a definir: ', (name) => {
+            require('../lib/functions.js').mcr(name)
+            rl.close();
+        });
+
+    }
+    else console.log("Comando no encontrado")
+}
+else if (args[0] == '--help' || args[0] == '-h') {
+    let package = require('../package.json');
     console.log(`
+    NeøTools v${package.version}
+    author: naguerodev@gmail.com
+
     Usage: neotools [command] [options]
 
     Commands:
-        start           Instala las herramientas necesarias para el proyecto
+        start           Instala herramientas y genera la estructura del proyecto
         --add           Agrega funcionalidades al proyecto
             logs        Agrega el logger al proyecto
             helpers     Agrega funciones comunes al proyecto
             dependences Agrega dependencias al proyecto
+        --generate      Genera elementos para el proyecto
+            model       Genera un modelo para el proyecto
+            controller  Genera un controlador para el proyecto
+            route       Genera una ruta para el proyecto
+            all         Genera un modelo, controlador y ruta para el proyecto
+        --version       Muestra la versión de neotools
+        --help          Muestra la ayuda de neotools
+
     `)
 } else {
     console.log("Comando no encontrado")
---
+++ bin/postinstall.js
@@ -0,0 +1,19 @@
+console.log(`
+    Greetings, I'm NeøTools, a CLI tool to help you with your NodeJS projects.
+    Thanks for using me, I hope you enjoy it.
+    You can find more information about me in my GitHub repository:
+    https://github.com/naguerovalencia/neotools-cli
+
+    For more help, use the command 'neotools --help' or 'neotools -h'
+
+
+         )  (
+        (   ) )
+         ) ( (
+       _______)_      
+    .-'---------|  
+   ( C|/\\/\\/\\/\\/|        ¡Happy Cøding 😊!
+    '-./\\/\\/\\/\\/|
+      '_________'
+       '-------'
+`) 
